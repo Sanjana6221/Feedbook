02b5a306fefd2e1e2b76ef22acdf7ffb502034f0
Sanjana <sanjnarathore1995@gmail.com>
2020-12-11 12:55:49 +0530

    CRUD comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,10 @@
 class Post < ApplicationRecord
   enum privacy: [:public, :private]
+
+  #Associations
+  has_many :comments, dependent: :destroy
+
+  #Validations
   validates :title, :description, :privacy, presence: true
   validates :description, length: { minimum: 20 }
 end
