f5381e4bc83765a961b77ee6fa56dc18c97c8e7a
Sanjana <sanjnarathore1995@gmail.com>
2020-12-11 23:08:01 +0530

    twitter bootstrap static view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,7 @@ class WelcomeController < ApplicationController
 	skip_before_action :authenticate_user!
 
 	def index
-		@posts = Post.where(status: "Public")
+		@posts = Post.where(status: "Public") unless current_user
+		@posts = Post.all if current_user
 	end
 end
